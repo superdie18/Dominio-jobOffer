a9c9c3d13bb5d4073bc2992854d00ab2a2d555f2
superdie18 <darojas.17@est.ucab.edu.ve>
2021-12-20 18:36:11 -0400

    Limpiando un poco

--- Domain/JobOffer/Entities/JobOfferLike.ts
+++ Domain/JobOffer/Entities/JobOfferLike.ts
@@ -1,5 +1,5 @@
 import IDomainEvent from "../../../shared/domain/IDomainEvent";
-import { IlikeAdded } from "../DomainEvent/ILikeAdded_DomainEvent";
+
 import LikeAdded_book from "../DomainEvent/LikeAdded_book";
 import JobOfferLikedId from "../ValueObjects/JobOfferLikeId";
 
@@ -8,30 +8,10 @@ import JobOfferLikedId from "../ValueObjects/JobOfferLikeId";
 
 export class JobOfferLike{
 	private eventRecorder: IDomainEvent[] = [];
-/*    public _id!: string;
-    private _value: boolean = false;
-	constructor(_id: string, _value: boolean, private readonly _jobOfferRepository: IlikeAdded){
-		this._id = _id;
-		this._value = _value ;
-	}
-*/	constructor(public id: JobOfferLikedId,
+
+	constructor(public id: JobOfferLikedId,
     			private value: boolean){};
-/*				
-	public get id(): string {
-		return this._id;
-	  }
-	public set id(id: string) {
-		this._id = id;
-	}
 
-	public get value(): boolean {
-		return this._value;
-	}
-	public set value(value: boolean) {
-		this._value = value;
-	}
-
-*/
 	public getEvents(){
 		return this.eventRecorder;
 	}
