3bf5abc3759b3da62f167788eedbe023c3319f9f
superdie18 <darojas.17@est.ucab.edu.ve>
2021-12-20 20:54:42 -0400

    test

--- test/JonOfferLike.test.ts
+++ test/JonOfferLike.test.ts
@@ -11,4 +11,12 @@ describe('Value Object likeID', () =>{
 	//	expect(JobOfferLikedId.create(id)).toBeInstanceOf(JobOfferLikedId);
 		expect(JobOfferLikedIdnew).toBeInstanceOf(JobOfferLike)
 	})
+	test('Should return the happy path (instance)',() =>{
+        
+		const id = new UniqueId().getId(); // structure uuid "a24a6ea4-ce75-4665-a070-57453082c256"
+        const JobOfferLikedIdnew2 = JobOfferLikedId.create(id);
+		const JobOfferLikedIdnew = JobOfferLike.aggreeliked(JobOfferLikedIdnew2)
+	//	expect(JobOfferLikedId.create(id)).toBeInstanceOf(JobOfferLikedId);
+		expect(JobOfferLikedIdnew).toBeInstanceOf(JobOfferLike)
+	})
 })
